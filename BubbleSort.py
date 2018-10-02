33695ac2e79e328b12f81bb63a86364e445ceed5
Wesley <wec137@ucsd.edu>
2018-10-02 14:27:30 -0700

    first commit

---
+++ BubbleSort.py
@@ -0,0 +1,27 @@
+import sys
+
+def BubbleSort(array):
+	counter = 0
+	i = 0
+	j = 0
+	n = len(array)
+	print("n = " + str(n))
+	while i < (n - 1):
+		#print("OUTER i is equal to " + str(i))
+		j = 0
+		while j < (n - i - 1):
+			#print("INNER j is equal to " + str(j))
+			if array[j] > array[j + 1]:
+				array[j], array[j + 1] = array[j + 1], array[j]
+			counter+=1	
+			j += 1
+		i += 1
+
+	print("sorted array: " + str(array))
+	return counter
+
+def main():
+  print("number of comparisons: " + str(BubbleSort([1, 2, 3, 4, 5, 6, 7])))
+  
+if __name__== "__main__":
+  main()
